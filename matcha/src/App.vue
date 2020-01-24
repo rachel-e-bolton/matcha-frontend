8af7a72049b264f59f7234afecb9a24991eeb8fc
Rachel Bolton <rachelbolton@Rachels-MacBook-Air.local>
2020-01-24 10:56:31 +0200

    Makes a basic matcha landing page, to start understanding passing props between components.

--- matcha/src/App.vue
+++ matcha/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" class="logo" src="./assets/logo-mustard.png">
+    <LandingPage msg="Welcome to matcha"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/LandingPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  width: 90vw;
 }
 </style>
