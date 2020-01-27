cfea6e91c2e8525757257197c57268c3d5bf30cf
Cameron <cdglanville@gmail.com>
2020-01-27 18:06:48 +0200

    added sign up buttons and navigation bar

--- matcha/src/components/LandingPage.vue
+++ matcha/src/components/LandingPage.vue
@@ -8,7 +8,24 @@
         <div>
             {{ msg }}
         </div>
+        <div class="signup-buttons">
+            <md-button :href="loginUrl" class="md-raised md-plain">
+                Log In
+            </md-button>
+            <md-button :href="loginUrl" class="md-raised md-plain">
+                Sign Up
+            </md-button>
+        </div>
+        <div class="phone-viewport">
+            <md-bottom-bar md-sync-route>
+                <md-bottom-bar-item to="/components/LandingPage" md-label="Profile" md-icon="profile"></md-bottom-bar-item>
+                <md-bottom-bar-item to="/components/LandingPage" md-label="Discover" md-icon="discover"></md-bottom-bar-item>
+                <md-bottom-bar-item to="/components/LandingPage" md-label="Search" md-icon="search"></md-bottom-bar-item>
+                <md-bottom-bar-item to="/components/LandingPage" md-label="Matches" md-icon="matches"></md-bottom-bar-item>
+            </md-bottom-bar>
+        </div>
     </div>
+    
 </template>
 
 <script>
@@ -20,5 +37,31 @@ export default {
 </script>
 
 <style>
-
+    .signup-buttons{
+        /* position: fixed;
+        transform: translate(-50%, 0);
+        left: 50%; */
+        display: inline-flex;
+        align-items: flex-end;
+        bottom: 200px;
+        padding: 0px;
+    }
+    .phone-viewport {
+        position: fixed;
+        transform: translate(-50%, 0);
+        left: 50%;
+        bottom: 0px;
+        padding: 0px;
+        width: 322px;
+        height: 60px;
+        display: inline-flex;
+        align-items: flex-end;
+        overflow: hidden;
+        border: 1px solid black;
+        background-color: white; /* rgba(white, .06); */
+        bottom: 0px;
+    }
+    .md-raised {
+        width: 33%;
+    }
 </style>
