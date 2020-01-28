9acc9d6981ffff3abd26cf401c1f7a50cd1841b3
Rachel Bolton <rachelbolton@Rachels-MacBook-Air.local>
2020-01-28 14:34:25 +0200

    Establishes generic positional template for all views.

--- matcha/src/components/LandingPage.vue
+++ matcha/src/components/LandingPage.vue
@@ -21,7 +21,7 @@
         </div>
         <div class="phone-viewport">
             <md-bottom-bar md-sync-route>
-                <md-bottom-bar-item to="/components/LandingPage" md-label="Profile" md-icon="profile"></md-bottom-bar-item>
+                <md-bottom-bar-item to="/about" md-label="Profile" md-icon="profile"></md-bottom-bar-item>
                 <md-bottom-bar-item to="/components/LandingPage" md-label="Discover" md-icon="discover"></md-bottom-bar-item>
                 <md-bottom-bar-item to="/components/LandingPage" md-label="Search" md-icon="search"></md-bottom-bar-item>
                 <md-bottom-bar-item to="/components/LandingPage" md-label="Matches" md-icon="matches"></md-bottom-bar-item>
--- matcha/src/views/SignUp.vue
+++ matcha/src/views/SignUp.vue
@@ -1,5 +1,88 @@
 <template>
-    <div class="signup">
-    A SIGN UP FORM GOES HERE
+  <div class="signup">
+    <div class="header">
+      Header Elements Here
+    </div>
+    <div class="content">
+      <SignUpForm />
+    </div>
+    <div class="footer">
+      Footer Elements Here
+    </div>
   </div>
 </template>
+
+<script>
+import SignUpForm from '../components/SignUpForm'
+
+export default {
+  name: 'signup',
+  components: {
+    SignUpForm
+  }
+}
+</script>
+
+<style>
+.signup {
+  height: 100%;
+}
+
+.header {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  height: 70px;
+  overflow: hidden;
+  background-color: #eec643;
+}
+
+
+.footer {
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  height: 70px;
+  overflow: hidden;
+  background-color: #eec643;
+}
+
+.content {
+  position: absolute;
+  top: 70px;
+  bottom: 70px;
+  right: 0px;
+  left: 0px;
+  overflow: auto;
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+.content::-webkit-scrollbar {
+    display: none;
+}
+
+/* Hide scrollbar for IE and Edge */
+.content {
+    -ms-overflow-style: none;
+}
+
+@media only screen and (min-width: 768px) and (max-width: 1024px) {
+  .header {
+    height: 120px;
+  }
+
+  .footer {
+    height: 120px;
+  }
+  
+  .content {
+    top: 120px;
+    bottom: 120px;
+  }
+}
+</style>
+
+
+
