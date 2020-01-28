34783a57bbf566b66df9bc859718a2726dc959aa
Rachel Bolton <rachelbolton@Rachels-MacBook-Air.local>
2020-01-28 09:54:19 +0200

    Adds vuex to current project.

--- matcha/src/main.js
+++ matcha/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import store from './store'
 
 Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
