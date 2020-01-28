05cb12aa6e3fdd24f5f4389a3ed35da4c2378afd
Rachel Bolton <rachelbolton@Rachels-MacBook-Air.local>
2020-01-28 09:58:01 +0200

    Adds vue-router to current project.

--- matcha/src/App.vue
+++ matcha/src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <div id="app" class="page-container">
-    <LandingPage msg="welcome to matcha."/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import LandingPage from './components/LandingPage.vue'
-
-export default {
-  name: 'app',
-  components: {
-    LandingPage
-  }
-}
-</script>
 
 <style>
 #app {
@@ -24,27 +17,16 @@ export default {
   color: #2c3e50;
 }
 
-.page-container {
-  height: 100%;
-  width: auto;
-  background-image: url("./assets/landing-background-mobile.jpg");
-  background-size: 150%;
-  background-position: center;
+#nav {
+  padding: 30px;
 }
 
-.logo {
-  width: 90vw;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-@media screen and (min-width: 1025px) {
-  .page-container {
-    background-image: url("./assets/landing-background-desktop.jpg");
-    background-size: 120%;
-  }
-  .logo {
-    width: 200px;
-  }
-  
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
-
 </style>
--- matcha/src/main.js
+++ matcha/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import store from './store'
+import router from './router'
 
 Vue.use(VueMaterial)
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
