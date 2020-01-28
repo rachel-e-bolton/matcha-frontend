93b98b67600dbe0be53c545e9948107942b90f74
Cameron <cdglanville@gmail.com>
2020-01-28 15:16:34 +0200

    moved header and footer from LandingPage to Home

--- matcha/src/components/LandingPage.vue
+++ matcha/src/components/LandingPage.vue
@@ -1,28 +1,16 @@
 <template>
     <div>
-        <Header/>
-        <div>
-           <h1>{{ msg }}</h1>
-        </div>
         <SigninButtons/>
-        <NavBar/>
     </div>
     
 </template>
 
 <script>
 import SigninButtons from './SigninButtons.vue'
-import NavBar from './NavBar.vue'
-import Header from './Header.vue'
 
 export default {
-    props: {
-        msg: String,
-    },
     components: {
-        Header,
-        SigninButtons,
-        NavBar
+        SigninButtons
     }
 }
 </script>
--- matcha/src/views/Home.vue
+++ matcha/src/views/Home.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
     <div class="header">
-      Header Elements Here
+      <Header/>
     </div>
     <div class="content">
-      <LandingPage />
+      <LandingPage/>
     </div>
     <div class="footer">
-      Footer Elements Here
+      <NavBar/>
     </div>
   </div>
 </template>
 
 <script>
 import LandingPage from '@/components/LandingPage.vue'
+import NavBar from '@/components/NavBar.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'home',
   components: {
-    LandingPage
+    Header,
+    LandingPage,
+    NavBar
   }
 }
 </script>
