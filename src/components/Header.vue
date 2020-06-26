56cd4012690ddda6423bac78459d6d1c99bc404a
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-06-26 08:27:10 +0200

    Minor visual clean-ups.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <md-toolbar class="md-transparent">
-            <router-link to="/"><img src="../assets/logo-white.png" alt="Matcha" width="200"></router-link>
+            <router-link class="logo-link" to="/"><img src="../assets/logo-plum.png" alt="Matcha" width="200"></router-link>
             <div class="md-toolbar-section-end">
                 <md-button to="/About" class="md-icon-button info">
                     <md-icon>info</md-icon>
