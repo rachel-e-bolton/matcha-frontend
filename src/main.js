99221823291fb2131033008dffdccbe81070a902
Glen Wasserfall <glen@wasserfalls.co.za>
2020-06-27 20:36:11 +0200

    added validation page and axios to Vue class

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/styles/custom.scss'
+import Axios from 'axios'
+
+const axios = require('axios').default;
 
 Vue.use(VueMaterial)
 Vue.use(BootstrapVue)
@@ -19,6 +22,8 @@ Vue.prototype.$store = Vue.observable({
   access_token: false,
   api : "http://192.168.88.251:5000/v1"
 })
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/views/ValidateEmail.vue
+++ src/views/ValidateEmail.vue
@@ -1,23 +1,61 @@
 <template>
-  <div>Verfifying Email..</div>
+  <div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center">
+      <div v-if="verifying">
+        <b-spinner style="width: 5rem; height: 5rem;" variant="primary"></b-spinner>
+        <div class="text-centered text-primary mt-4">Verifying your email</div>
+      </div>
+      <div v-if="failed">
+        <b-icon style="width: 5rem; height: 5rem;" class="text-primary" icon="x-circle"></b-icon>
+        <div class="text-centered text-primary mt-4 mb-4">{{ errorMessage }}</div>
+        <b-form inline>
+
+          <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+            <b-input id="inline-form-input-username" placeholder="Email"></b-input>
+          </b-input-group>
+
+          <b-button variant="primary">Resend Verification</b-button>
+        </b-form>
+
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 const axios = require('axios').default;
 
+
 export default {
+  data: function () {
+    return {
+      verifying : true,
+      failed : false,
+      errorMessage: ""
+    }
+  },
   mounted: function () {
-    // let code = this.$route.query.code
+    let code = this.$route.query.code
 
-    let test = this.$store
+    
 
-    console.log(test)
+    if (code) {
+      axios.get(`${this.$store.api}/validate/${code}`)
+        .then(res => {
 
-    // if (code) {
-    //   axios.get(`${this.$store.api}/validate/${code}`)
-    // } else {
-    //   alert("FAILED")
-    // }
+          this.$router.push("/")
+        })
+        .catch(error => {
+
+          this.verifying = false
+          this.failed = true
+
+          this.errorMessage = error.response.data.message
+        })
+    } else {
+      this.$router.push("/")
+    }
   }
 }
 </script>
