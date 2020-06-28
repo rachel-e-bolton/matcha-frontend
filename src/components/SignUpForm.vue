59260c9c2cd6b4b5a3380d1b694eb6b9a76e0f29
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-06-28 12:45:21 +0200

    Added login link from singup page.

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -92,8 +92,11 @@
               </p>
             </b-form-group>
 
+            <b-form-group class="d-flex justify-content-center">
+              <router-link class="ml-4 text-secondary display-8" to="/login">Already have an account? Login now.</router-link>
+            </b-form-group>
             <b-button class="float-right d-none d-md-block d-lg-block d-xl-block" variant="primary" type="submit">Submit</b-button>
-            <b-button class="btn-block d-md-none d-lg-none d-xl-none" variant="primary" type="submit">Submit</b-button>
+            <b-button class="btn-block d-md-none d-lg-none d-xl-none py-2" size="lg" variant="primary" type="submit">Submit</b-button>
 
           </b-form>
         </b-col>
