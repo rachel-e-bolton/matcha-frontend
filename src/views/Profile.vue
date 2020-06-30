450acfad2348b7afe65749e7d177d3bbc798fd4e
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-06-30 16:28:52 +0200

    Profile, Settings and Admin views registered and router guards added.

---
+++ src/views/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <div class="header">
+      <Header/>
+    </div>
+    <div class="content">
+
+    </div>
+    <div class="footer">
+      <NavBar/>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Header from '@/components/HeaderNav.vue'
+
+export default {
+  name: 'profile',
+  components: {
+    Header,
+    NavBar
+  }
+}
+</script>
+
+
+<style>
+@import '../assets/styles/ViewStyles.css';
+</style>
