ca1601500fa6aa1a691c686fa4648853eb0009e5
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-07-08 20:27:31 +0200

    Refactoring profile.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,7 +4,44 @@
       <Header/>
     </div>
     <div class="content">
-      <Profile v-bind:user="this.$store.user"/>
+      <div class="max-w-90 d-flex justify-content-center">
+        <b-col sm="auto" md="6" lg="4" class="rounded-lg shadow bg-light">
+          <div id="profile">
+            <div id="fame-rating">
+
+            </div>
+            <div id="photos">
+              
+            </div>
+            <div id="online">
+
+            </div>
+            <div id="personal-details">
+              <div id="full-name">
+                
+              </div>
+              <div id="age-gender">
+                
+              </div>
+            </div>
+            <div id="location">
+
+            </div>
+            <div id="bio">
+
+            </div>
+            <div id="interests">
+
+            </div>
+            <div id="sexual-preferences">
+
+            </div>
+            <div id="other-actions">
+             
+            </div>
+          </div>
+        </b-col>
+      </div>
     </div>
     <div class="footer">
       <NavBar/>
