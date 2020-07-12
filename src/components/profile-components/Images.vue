47df4b2c146dff70f70466465be7c43476f4d022
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-07-12 22:47:52 +0200

    Images upload and delete working.

--- src/components/profile-components/Images.vue
+++ src/components/profile-components/Images.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="container d-flex justify-content-center">
     <div style="width: 350px;" class="mb-4 position-relative">
-      <div v-if="loading" style="height: 400px;" class="d-flex justify-content-center align-items-center">
+      <div
+        v-if="loading"
+        style="height: 400px;"
+        class="d-flex justify-content-center align-items-center"
+      >
         <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
       </div>
       <div v-else>
-        <div v-if="imageSrc" class="d-flex justify-content-center">
+        <div v-if="imageSrc.length > 0" class="d-flex justify-content-center">
           <hooper style="height: 400px; width: 300px">
-            <slide v-for="(image,index) in imageSrc" :key="index" class="rounded-lg shadow-sm">
-              <img :src="image.image64" class="rounded-lg shadow-sm">
+            <slide
+              v-for="(image, index) in imageSrc"
+              :key="index"
+              class="rounded-lg shadow-sm"
+            >
+              <img :src="image.image64" class="rounded-lg shadow-sm" />
             </slide>
             <hooper-navigation slot="hooper-addons"></hooper-navigation>
           </hooper>
@@ -16,50 +24,110 @@
         <div v-else class="d-flex justify-content-center">
           <hooper style="height: 400px; width: 300px">
             <slide class="rounded-lg shadow-sm">
-              <img src="@/assets/no-photo.png" class="rounded-lg shadow-sm">
+              <img src="@/assets/no-photo.png" class="rounded-lg shadow-sm" />
             </slide>
           </hooper>
         </div>
-      </div>        
+      </div>
       <div>
-        <b-button 
-          style="position: absolute; right: -5px; bottom: -10px; z-index: 1000" 
-          pill 
+        <b-button
+          style="position: absolute; right: -5px; bottom: -10px; z-index: 1000"
+          pill
           class="bg-secondary display-1 shadow"
           size="lg"
-          @click="showModal('uploads')">
+          @click="showModal('uploads')"
+        >
           <b-icon variant="light" class="mb-1 mt-2" icon="pencil"></b-icon>
         </b-button>
-        <b-modal ref="uploads" hide-footer title="Upload or Change Photos" class="text-center">
+        <b-modal
+          ref="uploads"
+          hide-footer
+          title="Upload or Change Photos"
+          class="text-center"
+        >
           <div v-if="uploading">
-            <vue-croppie ref="croppieRef" 
-              :boundary="{ width: 300, height: 400}" 
-              :viewport="{ width:300, height:400, 'type':'square' }"
+            <vue-croppie
+              ref="croppieRef"
+              :boundary="{ width: 300, height: 400 }"
+              :viewport="{ width: 300, height: 400, type: 'square' }"
               :showZoomer="false"
               :enableResize="false"
-              class="position-relative">
+              class="position-relative"
+            >
             </vue-croppie>
-            <b-button 
-              style="font-size: 2rem; border: none; position: absolute; right: 80px; bottom: 15px; z-index: 1000" 
-              variant="primary" class="rounded-pill lg shadow-lg pt-2" @click="crop">
+            <b-button
+              style="font-size: 2rem; border: none; position: absolute; right: 80px; bottom: 15px; z-index: 1000"
+              variant="primary"
+              class="rounded-pill lg shadow-lg pt-2"
+              @click="crop"
+            >
               <b-icon icon="check2-circle"></b-icon>
             </b-button>
           </div>
           <div v-else>
-            <div v-if="imageSrc" class="d-flex justify-content-center flex-wrap">
-              <div v-for="(image,index) in imageSrc" :key="index" class="mx-2 mb-3 mt-2 rounded-lg" style="width: 80px">
-                <img :src="image.image64" style="width: 80px" class="rounded-lg"/>
+            <div
+              v-if="imageSrc.length > 0"
+              class="d-flex justify-content-center flex-wrap mx-md-5"
+            >
+              <div
+                v-for="(image, index) in imageSrc"
+                :key="index"
+                class="mx-2 mb-3 mt-2 rounded-lg"
+                style="width: 80px"
+              >
+                <div class="position-relative"
+                style="font-size: 1.5rem">
+                  <img
+                    :src="image.image64"
+                    style="width: 80px"
+                    class="rounded-lg"
+                  />
+                  <b-icon
+                    style="border: none; position: absolute; right: 5px; bottom: 5px; z-index: 1000"
+                    variant="danger"
+                    class="rounded-pill lg shadow-lg bg-light"
+                    @click="removeImage(image.id)"
+                    icon="x-circle"
+                  ></b-icon>
+                </div>
               </div>
-              <div v-if="imageSrc.length < 5" class="mx-2 mb-3 mt-2 d-flex align-items-center justify-content-center border border-primary rounded-lg bg-light" style="width: 80px">
-                <label for="img" style="font-size: 3rem" class=""><b-icon variant="primary" icon="plus-circle"></b-icon><span style="font-size: 1rem"></span></label>                  
+              <div
+                v-if="imageSrc.length < 5"
+                class="mx-2 mb-3 mt-2 d-flex align-items-center justify-content-center border border-primary rounded-lg bg-light"
+                style="width: 80px"
+              >
+                <label for="img" style="font-size: 3rem" class=""
+                  ><b-icon variant="primary" icon="plus-circle"></b-icon
+                  ><span style="font-size: 1rem"></span
+                ></label>
               </div>
               <div v-else>
-                <div class="mx-2 mb-3 mt-2 d-flex align-items-center justify-content-center text-primary" style="width: 80px, height: auto">
-                  <span class="small">Maximum pictures reached.</span>             
+                <div
+                  class="mx-2 mb-3 mt-2 d-flex align-items-center justify-content-center text-primary"
+                  style="width: 80px, height: auto"
+                >
+                  <span class="small">Maximum pictures reached.</span>
                 </div>
               </div>
             </div>
-            <input type="file" @change="croppie" name="uploadfile" id="img" style="display:none;" accept=".jpg, .png"/>
+            <div
+              v-else
+              class="mx-2 mb-3 mt-2 d-flex align-items-center justify-content-center border border-primary rounded-lg bg-light"
+              style="width: 80px"
+            >
+              <label for="img" style="font-size: 3rem" class=""
+                ><b-icon variant="primary" icon="plus-circle"></b-icon
+                ><span style="font-size: 1rem"></span
+              ></label>
+            </div>
+            <input
+              type="file"
+              @change="croppie"
+              name="uploadfile"
+              id="img"
+              style="display:none;"
+              accept=".jpg, .png"
+            />
           </div>
         </b-modal>
       </div>
@@ -68,14 +136,14 @@
 </template>
 
 <script>
-import { Hooper, Slide, Navigation as HooperNavigation } from 'hooper';
-import 'hooper/dist/hooper.css';
+import { Hooper, Slide, Navigation as HooperNavigation } from "hooper";
+import "hooper/dist/hooper.css";
 
 export default {
   components: {
     Hooper,
     Slide,
-    HooperNavigation
+    HooperNavigation,
   },
   data() {
     return {
@@ -85,109 +153,149 @@ export default {
       upload: {
         id: null,
         is_primary: true,
-        image_type: '',
-        image64: '',
+        image_type: "",
+        image64: "",
       },
-      uploading: false
-    }
+      uploading: false,
+    };
   },
   methods: {
-    getBase64: function () {
+    getBase64: function() {
       if (this.images64) {
-        this.images64 = _.orderBy(this.images64, 'image64', 'desc')
-      }
-
-      this.imageSrc = this.images64.filter(function(image) {
-        return image.image64.length > 0
-      })
-
-      if (this.imageSrc) {
-        this.imageSrc.forEach(image => {
-          image.image64 = "data:image/" + image.image_type + ";base64, " + image.image64
+        this.imageSrc = this.images64.filter(function(image) {
+          return image.image64 != '&nbsp'
         });
       }
-      this.loading = false
+
+      if (this.imageSrc) {
+        this.imageSrc.forEach((image) => {
+          image.image64 =
+            "data:image/" + image.image_type + ";base64, " + image.image64;
+        });
+      }
+      this.loading = false;
     },
 
-          showModal(ref) {
-        this.$refs[ref].show()
-      },
+    showModal(ref) {
+      this.$refs[ref].show();
+    },
 
-      uploadImage: function () {
-        // let self = this
-        // let empties = this.images64.filter(function(image) {
-        //   return image.image64.length === 0 
-        // })
+    uploadImage: function() {
+      if (this.images64) {
+        this.images64.forEach((image, array, index) => {
+          if (image.is_primary) {
+            this.upload.is_primary = false;
+          }
+          if (image.image64 === "&nbsp") {
+            this.upload.id = image.id;
+            this.upload.is_primary = image.is_primary;
+          }
+        });
+      }
 
-        // if (empties.length > 0) {
-        //   const isPrimary = (image) => image.is_primary === true
-        //   if ((primaryIndex = empties.findIndex(isPrimary))) {
-
-        //   }
-        // }
-
-        this.$http.put(`${this.$api}/images`, this.upload)
-        .then(res => { 
-          this.images64.push(this.upload)
-          this.getBase64()
-          this.$bvToast.toast('Image uploaded.', {
+      this.$http
+        .put(`${this.$api}/images`, this.upload)
+        .then((res) => {
+          this.imageSrc = [];
+          this.loadUser();
+          let test = this.imageSrc;
+          this.$bvToast.toast("Image uploaded.", {
             title: "Success!",
             autoHideDelay: 2000,
             toaster: "b-toaster-top-center",
             variant: "info",
             noCloseButton: true,
-          })
-          self.getUser()
+          });
         })
-        .catch(err => {
+        .catch((err) => {
           this.$bvToast.toast(err.response.data.message, {
             title: "Error!",
             autoHideDelay: 5000,
-            variant: "danger"
-          })
+            variant: "danger",
+          });
+        });
+    },
+
+    removeImage: function(id) {
+      this.upload.id = id;
+      this.upload.image64 = "&nbsp";
+      this.upload.image_type = "&nbsp";
+
+      this.$http
+        .put(`${this.$api}/images`, this.upload)
+        .then((res) => {
+          this.imageSrc = [];
+          this.loadUser();
+          this.$bvToast.toast("Image deleted.", {
+            title: "Success!",
+            autoHideDelay: 2000,
+            toaster: "b-toaster-top-center",
+            variant: "info",
+            noCloseButton: true,
+          });
         })
-      },
-    
-      croppie: function (e) {
-        this.uploading = true
-        var files = e.target.files || e.dataTransfer.files;
-        if (!files.length) return;
-
-        const name = e.target.files[0].name;
-        const lastDot = name.lastIndexOf('.');
-        const ext = name.substring(lastDot + 1);
-
-        this.extension = ext;
-
-        var reader = new FileReader();
-        reader.onload = e => {
-          this.$refs.croppieRef.bind({
-            url: e.target.result
+        .catch((err) => {
+          this.$bvToast.toast(err.response.data.message, {
+            title: "Error!",
+            autoHideDelay: 5000,
+            variant: "danger",
           });
-        };
+        });
+    },
 
-        reader.readAsDataURL(files[0]);
-      },
+    croppie: function(e) {
+      this.uploading = true;
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
 
-      crop: function () {
-        let options = {
-          type: 'base64',
-          size: { width: 300, height: 400 },
-          format: this.extension
-        };
+      const name = e.target.files[0].name;
+      const lastDot = name.lastIndexOf(".");
+      const ext = name.substring(lastDot + 1);
 
-        this.$refs.croppieRef.result(options, output => {
-          const base64 = output.substring(output.lastIndexOf(',') + 1)          
-          this.upload.image64 = base64
-          this.upload.image_type = this.extension
-          this.uploadImage()
-          });
-        this.uploading = false
-      }
+      this.extension = ext;
+
+      var reader = new FileReader();
+      reader.onload = (e) => {
+        this.$refs.croppieRef.bind({
+          url: e.target.result,
+        });
+      };
+
+      reader.readAsDataURL(files[0]);
+    },
+
+    crop: function() {
+      let options = {
+        type: "base64",
+        size: { width: 300, height: 400 },
+        format: this.extension,
+      };
+
+      this.$refs.croppieRef.result(options, (output) => {
+        const base64 = output.substring(output.lastIndexOf(",") + 1);
+        this.upload.image64 = base64;
+        this.upload.image_type = this.extension;
+        this.uploadImage();
+      });
+      this.uploading = false;
+    },
+
+    loadUser: function() {
+      this.$http
+        .get(`${this.$api}/user/${this.$store.user.id}`)
+        .then((res) => {
+          this.images64 = _.cloneDeep(res.data.images);
+          this.getBase64();
+          console.log(this.images64);
+          if (this.images64[0].is_primary) {
+            console.log("true");
+          }
+        })
+        .catch((err) => {});
+    },
   },
   created() {
-    this.images64 = _.cloneDeep(this.$store.user.images)
-    this.getBase64()
-  }
-}
+    this.loadUser();
+  },
+};
 </script>
